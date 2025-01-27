820d4fe8b168b841313d1f1e5795412bac163ca5
Hari <reachk.hariharan@gmail.com>
2025-01-27 14:53:59 +0000

    modifed tpye command

--- src/main/java/shell/command/TypeCommand.java
+++ src/main/java/shell/command/TypeCommand.java
@@ -1,5 +1,6 @@
 package shell.command;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,15 +15,36 @@ public class TypeCommand implements ICommand {
 
     @Override
     public void execute(String[] args) {
-        if (args.length > 1) {
-            String command = args[1];
-            if (BUILTIN_COMMANDS.contains(command)) {
-                System.out.println(command + " is a shell builtin");
-            } else {
-                System.out.println(command + ": not found");
-            }
-        } else {
+        if (args.length < 2) {
             System.out.println("type: missing operand");
+            return;
         }
+
+        String command = args[1];
+
+        // Check if it's a shell builtin
+        if (BUILTIN_COMMANDS.contains(command)) {
+            System.out.println(command + " is a shell builtin");
+            return;
+        }
+
+        // Check PATH environment variable for executables
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv == null || pathEnv.isEmpty()) {
+            System.out.println(command + ": not found");
+            return;
+        }
+
+        String[] paths = pathEnv.split(":");
+        for (String path : paths) {
+            File file = new File(path, command);
+            if (file.exists() && file.canExecute()) {
+                System.out.println(command + " is " + file.getAbsolutePath());
+                return;
+            }
+        }
+
+        // If command is not found in PATH directories
+        System.out.println(command + ": not found");
     }
 }
