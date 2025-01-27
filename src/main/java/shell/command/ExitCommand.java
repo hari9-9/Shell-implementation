1f418488e76e53f557fcaafffa9cea34c9523248
Hari <reachk.hariharan@gmail.com>
2025-01-27 14:29:09 +0000

    refactoring for OOD

--- src/main/java/shell/command/ExitCommand.java
+++ src/main/java/shell/command/ExitCommand.java
@@ -3,7 +3,6 @@ package shell.command;
 public class ExitCommand implements ICommand {
     @Override
     public void execute(String[] args) {
-        System.out.println("Exiting...");
         System.exit(0);
     }
 }
